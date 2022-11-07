fc7e2dbe3a131b53dd5a73335ef748e9c845feb5
Ahmad Ihsan <ihsanashi@gmail.com>
2022-11-07 10:44:27 +0800

    feat: create asset type

---
+++ schemas/asset.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'asset',
+  type: 'document',
+  title: 'Asset',
+  fields: [
+    {
+      name: 'caption',
+      title: 'Caption',
+      type: 'string',
+    },
+  ],
+};
