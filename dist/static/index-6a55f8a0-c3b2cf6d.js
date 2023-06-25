b3c7d477873cfd1f116b323305ee27d6201601f7
Ahmad Ihsan <ihsanashi@gmail.com>
2023-06-25 18:00:02 +0800

    chore: build studio

--- dist/static/index-6a55f8a0-c3b2cf6d.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-2cbd8830-e64e5e8c.js";import"./sanity-7532b75c.js";import"./index-ff380dfe.js";export{m as DocumentPaneProvider,a as default};
---
+++ dist/static/index-aa34881c-c9e43eb0.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-37eb7c2e-1216c7f7.js";import"./sanity-f6df1d37.js";import"./index-12a2dbdc.js";export{m as DocumentPaneProvider,a as default};
