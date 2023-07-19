fc1f33cfec0420970755882d365c319bd194761e
Ahmad Ihsan <ihsanashi@gmail.com>
2023-07-19 08:18:14 +0800

    feat: add list preview

--- schemas/igStoryClip.jsx
+++ schemas/igStoryClip.jsx
@@ -1,7 +1,31 @@
+import React from 'react';
+
 export default {
   name: 'igStoryClip',
   title: 'IG Story Clip',
   type: 'document',
+  preview: {
+    select: {
+      _id: '_id',
+      path: 'path',
+    },
+    prepare(selection) {
+      const { _id, path } = selection;
+      return {
+        title: _id,
+        subtitle: `${path.replace('instagram/stories/', '')}`,
+        media: (
+          <video height="200" width="200" muted>
+            <source
+              src={`https://cdn.sanity.io/files/2ciw864r/production/${_id}.mp4`}
+              type="video/mp4"
+            />
+            Your browser does not support video tag.
+          </video>
+        ),
+      };
+    },
+  },
   fields: [
     {
       name: 'file',
