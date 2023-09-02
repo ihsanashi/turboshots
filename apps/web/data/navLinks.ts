22ae0cc48bd28a5c88261487c611bcaea58a2bae
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-02 17:27:46 +0800

    feat: add site navigation links

---
+++ apps/web/data/navLinks.ts
@@ -0,0 +1,23 @@
+type NavLink = {
+  href: string;
+  title: string;
+};
+
+export const navLinks: NavLink[] = [
+  {
+    href: '/',
+    title: 'Home',
+  },
+  {
+    href: '/',
+    title: 'Feed',
+  },
+  {
+    href: '/stories',
+    title: 'Stories',
+  },
+  {
+    href: '/about',
+    title: 'About',
+  },
+];
