20a87a9b2babe2f3804846b2bf9350aa03b393cf
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-02 17:27:21 +0800

    feat: add site metadata details

---
+++ apps/web/data/siteMetadata.js
@@ -0,0 +1,16 @@
+export const siteMetadata = {
+  title: 'Shots',
+  author: 'Ahmad Ihsan',
+  headerTitle: 'Shots | by Ahmad Ihsan',
+  description:
+    'An online gallery of media content retrieved from my Instagram account',
+  language: 'en-US',
+  theme: 'system',
+  siteUrl: '',
+  siteRepo: '',
+  siteLogo: '',
+  email: 'ihsanashi@gmail.com',
+  github: '',
+  linkedin: '',
+  locale: 'en-US',
+};
