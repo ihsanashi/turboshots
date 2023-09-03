a29cb4a4fb9d686bffcb81703edc864b3b4582c1
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-03 17:51:44 +0800

    feat: test out components from ui library

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { Button, Paragraph } from 'ui'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-between p-24">
-      <h1>Hello world</h1>
+    <div className="flex flex-col items-center space-y-2 justify-between p-24">
+      <h1 className='font-bold text-2xl'>Hello world</h1>
+      <Paragraph text='Well hello there' />
+      <Button>Boop</Button>
     </div>
   )
 }
