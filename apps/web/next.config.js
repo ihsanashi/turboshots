215dfd8c3ed3fa4cebc38bcae4bbfd1fd1208b4b
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-07 21:32:12 +0800

    feat: transpile sanity-config package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['sanity-config'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
