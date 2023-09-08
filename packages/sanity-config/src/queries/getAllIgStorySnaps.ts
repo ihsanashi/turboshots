26f96b84a618d9c5a1f8580da60f8cf29e28d348
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-08 09:15:30 +0800

    feat: add starter graphql queries for image types

---
+++ packages/sanity-config/src/queries/getAllIgStorySnaps.ts
@@ -0,0 +1,30 @@
+import { gql } from '@apollo/client';
+
+export const GET_ALL_IG_STORY_SNAPS = gql`
+  query allIgStorySnaps($offset: Int) {
+    allIgStorySnap(sort: { taken_at: ASC }, limit: 10, offset: $offset) {
+      _id
+      _createdAt
+      _updatedAt
+      image {
+        asset {
+          _id
+          _createdAt
+          _updatedAt
+          label
+          title
+          description
+          altText
+          extension
+          path
+          url
+        }
+      }
+      path
+      caption
+      location
+      visible
+      taken_at
+    }
+  }
+`;
