a7ecf45daf49add83e1e0f85d916e488924450c4
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-08 09:15:02 +0800

    chore: use env variables

--- packages/sanity-config/src/sanity.config.ts
+++ packages/sanity-config/src/sanity.config.ts
@@ -3,8 +3,8 @@ import { schemas } from './schemas/schema';
 
 export const config = defineConfig({
   title: 'Shots | Studio',
-  projectId: '2ciw864r',
-  dataset: 'production',
+  projectId: process.env.SANITY_PROJECT_ID as string,
+  dataset: process.env.SANITY_DATASET as string,
   apiVersion: '2023-19-08',
   basePath: '/studio',
   schema: {
