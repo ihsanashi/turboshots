2c3a1b590c002baac4a3bd2f0872a5072bc64ac8
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-09 14:48:53 +0800

    feat: add groq and graphql queries

---
+++ packages/sanity-config/src/groq-queries/igStoryClips.ts
@@ -0,0 +1,6 @@
+import groq from 'groq';
+
+export const GET_IG_STORY_CLIPS_OFFSET_BY_10 = groq`*[_type == "igStoryClip"] | order(taken_at desc)[0...10]`;
+
+export const GET_IG_STORY_CLIPS_COUNT =
+  groq`count(*[_type == 'igStoryClip'])` as unknown as number;
