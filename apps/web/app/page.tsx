44f933519f4886bd5a95b54240b41497f660b742
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-11 18:55:25 +0800

    feat: display igPhotos, remove old UI

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,86 +1,25 @@
-'use client';
+import Image from 'next/image';
+import { getIgPhotos } from '../../../packages/sanity-config/src/functions';
 
-import { Button, Paragraph, Select } from 'ui';
-
-export default function Home() {
-  const fruits = [
-    {
-      label: 'Apple',
-      value: 'apple',
-      disabled: false,
-    },
-    {
-      label: 'Banana',
-      value: 'banana',
-      disabled: false,
-    },
-    {
-      label: 'Blueberry',
-      value: 'blueberry',
-      disabled: false,
-    },
-    {
-      label: 'Grapes',
-      value: 'grapes',
-      disabled: true,
-    },
-    {
-      label: 'Pineapple',
-      value: 'pineapple',
-      disabled: false,
-    },
-  ];
-
-  const vegetables = [
-    {
-      label: 'Potato',
-      value: 'potato',
-      disabled: false,
-    },
-    {
-      label: 'Spinach',
-      value: 'spinach',
-      disabled: false,
-    },
-  ];
-
-  const proteins = [
-    {
-      label: 'Chicken',
-      value: 'chicken',
-      disabled: false,
-    },
-    {
-      label: 'Meat',
-      value: 'meat',
-      disabled: true,
-    },
-    {
-      label: 'Salmon',
-      value: 'salmon',
-      disabled: false,
-    },
-  ];
+export default async function Home() {
+  const photos = await getIgPhotos();
 
   return (
     <div className='flex flex-col items-center justify-between space-y-5 p-24'>
-      <h1 className='text-2xl font-bold'>Hello world</h1>
-      <Paragraph text='Well hello there' />
-      <Button>Boop</Button>
-      <Select
-        placeholder='Select one'
-        ariaLabel='Select menu'
-        size='medium'
-        intent='fill'
-      >
-        <Select.Content>
-          <Select.Group label='Fruits' items={fruits} />
-          <Select.Separator />
-          <Select.Group label='Vegetables' items={vegetables} />
-          <Select.Separator />
-          <Select.Group label='Proteins' items={proteins} />
-        </Select.Content>
-      </Select>
+      <ul className='grid grid-cols-3 gap-5'>
+        {photos.map((photo) => (
+          <li key={photo._id}>
+            <Image
+              className='h-48 w-96 object-cover'
+              src={photo.image}
+              alt={photo.caption ?? `Image`}
+              height={300}
+              width={300}
+              objectFit=''
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
