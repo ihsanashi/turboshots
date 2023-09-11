9ba66dc0f5b1d86fb57dbcb8268be694b2a5a01c
Ahmad Ihsan <ihsanashi@gmail.com>
2023-09-11 18:54:26 +0800

    feat: modify returned igPhoto object

--- packages/sanity-config/src/groq-queries/igPhotos.ts
+++ packages/sanity-config/src/groq-queries/igPhotos.ts
@@ -1,6 +1,18 @@
 import groq from 'groq';
 
-export const GET_IG_PHOTOS_OFFSET_BY_10 = groq`*[_type == "igPhoto"] | order(takenAt desc)[0...10]`;
+export const GET_IG_PHOTOS_OFFSET_BY_10 = groq`
+  *[_type == "igPhoto"]{
+    _id,
+    _createdAt,
+    _updatedAt,
+    caption,
+    "image": image.asset->url,
+    location,
+    path,
+    "slug": slug.current,
+    takenAt,
+    visible
+  } | order(takenAt desc)[0...10]`;
 
 export const GET_IG_PHOTOS_COUNT =
   groq`count(*[_type == 'igPhoto'])` as unknown as number;
